31ba492d40e473b412f672853b2587d1313e405b
Rhys Bowen Harries <rbharries@gmail.com>
2019-04-08 19:05:22 +0100

    team MVP

--- specs/sports_teams_specs.rb
+++ specs/sports_teams_specs.rb
@@ -5,30 +5,34 @@ require_relative('../sports_teams')
 class TestSportsTeam < MiniTest::Test
 
     def test_team_name_search
-      sports_team = SportsTeam.new("Manchester United", ["David Hasselhof", "Zoe Ball", "Kermit T Frog" ], "Terry McTerry")
+      sports_team = SportsTeam.new("Manchester United", ["David Hasselhof", "Zoe Ball", "Kermit T Frog" ], "Terry McTerry", 0)
       assert_equal("Manchester United", sports_team.team_name)
     end
 
 
   def test_coach_name_change
-    sports_team = SportsTeam.new("Manchester United", ["David Hasselhof", "Zoe Ball", "Kermit T Frog" ], "Terry McTerry")
+    sports_team = SportsTeam.new("Manchester United", ["David Hasselhof", "Zoe Ball", "Kermit T Frog" ], "Terry McTerry", 0)
     sports_team.coach = "Jackie Chan"
     assert_equal("Jackie Chan", sports_team.coach)
   end
 
   def test_add_player_to_player_array
-    sports_team = SportsTeam.new("Manchester United", ["David Hasselhof", "Zoe Ball", "Kermit T Frog" ], "Terry McTerry")
+    sports_team = SportsTeam.new("Manchester United", ["David Hasselhof", "Zoe Ball", "Kermit T Frog" ], "Terry McTerry", 0)
     sports_team.add_player_to_player_array("Mrs Piggy")
     assert_equal(4, sports_team.players.length)
 
   end
 
   def test_is_player_in_team__true
-    sports_team = SportsTeam.new("Manchester United", ["David Hasselhof", "Zoe Ball", "Kermit T Frog" ], "Terry McTerry")
+    sports_team = SportsTeam.new("Manchester United", ["David Hasselhof", "Zoe Ball", "Kermit T Frog" ], "Terry McTerry", 0)
     sports_team.is_player_in_team("Zoe Ball")
     assert_equal(true, sports_team.is_player_in_team("Zoe Ball"))
   end
 
-
+  def test_team_win_point_adder
+    sports_team = SportsTeam.new("Manchester United", ["David Hasselhof", "Zoe Ball", "Kermit T Frog" ], "Terry McTerry", 0)
+    sports_team.team_win_point_adder("Win")
+    assert_equal(1, sports_team.points)
+  end
 
 end
--- sports_teams.rb
+++ sports_teams.rb
@@ -1,11 +1,12 @@
 class SportsTeam
 
-  attr_accessor :team_name, :players, :coach
+  attr_accessor :team_name, :players, :coach, :points
 
-    def initialize(team_name, players, coach)
+    def initialize(team_name, players, coach, points)
       @team_name = team_name
       @players = players
       @coach = coach
+      @points = points
     end
 
 
@@ -15,6 +16,12 @@ class SportsTeam
 
     def is_player_in_team(player)
       @players.include?(player)
+    end
+
+    def team_win_point_adder(result)
+      if result == "Win"
+        @points += 1
+      end  
     end
 
 end
